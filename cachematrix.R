b716369e95aae13c16c989c12cc278917efdab73
evrik <evrik@users.noreply.github.com>
2014-05-25 16:28:36 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,17 @@
 ## This function creates a special "matrix" object that can cache its inversehead
-
-makeCacheMatrix <- function(x = matrix()) { #creating a list of functions
+makeCacheMatrix <- function(x = matrix()) {
                  m <- NULL # add internal variable
-        set <- function(y) { #subfunction, it passes 
+        set <- function(y) { 
+#subfunction takes the matrix and assigns it to a variable x 
+# then the subfuction clears the cache (m)
+# we use <<- operator to clean x and m variables in parental enviroment as well
                 x <<- y
                 m <<- NULL
         }
-        get <- function() x
-        setsolve <- function(solve) m <<- solve
-        getsolve <- function() m
+        get <- function() x # "get" subfunction takes matrix, that was stored in x and returns it.
+        setsolve <- function(solve) m <<- solve #subfuction calculates inverse matrix and stores it in setsolve 
+        getsolve <- function() m #subfunction just return cached(stored) inverse matrix
+# creating a list of the 4 subfunctions defined before to use them later from cachesolve function        
         list(set = set, get = get,
              setsolve = setsolve,
              getsolve = getsolve)
@@ -25,9 +28,6 @@ cachesolve <- function(x, ...) {
         m
 }
 }
-
-
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
